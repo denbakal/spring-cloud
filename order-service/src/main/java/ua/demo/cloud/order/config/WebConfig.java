221661bab21b3ae4b41c81d44ec29811c40d15b2
denbakal <denbakal7@gmail.com>
2018-12-14 00:27:11 +0200

    Communications: RESTTemplate

--- order-service/src/main/java/ua/demo/cloud/order/config/WebConfig.java
+++ order-service/src/main/java/ua/demo/cloud/order/config/WebConfig.java
@@ -1,5 +1,6 @@
 package ua.demo.cloud.order.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,7 +8,10 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class WebConfig {
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder
+                .setConnectTimeout(2000)
+                .setReadTimeout(5000)
+                .build();
     }
 }
