91aecd78aad6051fb9426244b2949ece22ca7a9d
denbakal <denbakal7@gmail.com>
2018-12-16 23:39:37 +0200

    Service discovery: Spring Eureka (service registry)

--- order-service/src/main/java/ua/demo/cloud/order/OrderServiceApplication.java
+++ order-service/src/main/java/ua/demo/cloud/order/OrderServiceApplication.java
@@ -2,8 +2,12 @@ package ua.demo.cloud.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableEurekaClient
 public class OrderServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
--- order-service/src/main/java/ua/demo/cloud/order/feign/PaymentClient.java
+++ order-service/src/main/java/ua/demo/cloud/order/feign/PaymentClient.java
@@ -1,9 +1,11 @@
 package ua.demo.cloud.order.feign;
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import ua.demo.cloud.order.dto.PaymentDto;
 
+@FeignClient("payment-service")
 public interface PaymentClient {
     @RequestMapping(method = RequestMethod.POST, value = "/payments", consumes = "application/json")
     PaymentDto createPayment(PaymentDto paymentDto);
