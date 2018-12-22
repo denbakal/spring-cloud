b139ae4a87fb7ab9ffa83881f47042a9f90dd734
denbakal <denbakal7@gmail.com>
2018-12-22 20:48:05 +0200

    Load Balancing: Spring Ribbon + Feign (without service discovery)

--- order-service/src/main/java/ua/demo/cloud/order/OrderServiceApplication.java
+++ order-service/src/main/java/ua/demo/cloud/order/OrderServiceApplication.java
@@ -2,11 +2,13 @@ package ua.demo.cloud.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import ua.demo.cloud.order.config.RibbonConfiguration;
 
 @SpringBootApplication
 @EnableFeignClients
+@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
 public class OrderServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
---
+++ order-service/src/main/java/ua/demo/cloud/order/config/RibbonConfiguration.java
@@ -0,0 +1,21 @@
+package ua.demo.cloud.order.config;
+
+import com.netflix.loadbalancer.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RibbonConfiguration {
+    @Bean
+    public IPing ping() {
+        // will ping every URL to determine the server’s availability
+        return new PingUrl();
+    }
+
+    @Bean
+    public IRule rule() {
+        // each server is given a weight according to its average response time,
+        // lesser the response time gives lesser the weight.
+        return new WeightedResponseTimeRule();
+    }
+}
