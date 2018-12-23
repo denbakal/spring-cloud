62e3263aa1e299401fb5ca1fdd1c4e97fa42d278
denbakal <denbakal7@gmail.com>
2018-12-23 12:30:33 +0200

    Load Balancing: Spring Ribbon + Feign (with service discovery)

--- order-service/src/main/java/ua/demo/cloud/order/OrderServiceApplication.java
+++ order-service/src/main/java/ua/demo/cloud/order/OrderServiceApplication.java
@@ -2,12 +2,14 @@ package ua.demo.cloud.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import ua.demo.cloud.order.config.RibbonConfiguration;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 @RibbonClients(defaultConfiguration = RibbonConfiguration.class)
 public class OrderServiceApplication {
     public static void main(String[] args) {
