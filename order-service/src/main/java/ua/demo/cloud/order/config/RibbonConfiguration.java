ac3ca5801b57a66e26af8f64b8fded00eb881332
denbakal <denbakal7@gmail.com>
2018-12-24 09:29:32 +0200

    Load Balancing: Spring Ribbon + Feign (with service discovery)

--- order-service/src/main/java/ua/demo/cloud/order/config/RibbonConfiguration.java
+++ order-service/src/main/java/ua/demo/cloud/order/config/RibbonConfiguration.java
@@ -1,22 +1,26 @@
 package ua.demo.cloud.order.config;
 
+import com.netflix.client.config.IClientConfig;
 import com.netflix.loadbalancer.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 public class RibbonConfiguration {
+    @Autowired
+    IClientConfig ribbonClientConfig;
+
     @Bean
-    public IPing ping() {
+    public IPing ribbonPing(final IClientConfig config) {
         // will ping every URL to determine the server’s availability
-        return new PingUrl();
+        // return new PingUrl();
+        return new NoOpPing();
     }
 
     @Bean
-    public IRule rule() {
+    public IRule ribbonRule(final IClientConfig config) {
         // each server is given a weight according to its average response time,
         // lesser the response time gives lesser the weight.
         // return new WeightedResponseTimeRule();
-        return new ZoneAvoidanceRule();
+        return new RoundRobinRule();
     }
 }
