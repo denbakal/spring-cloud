75976e703fd545ef5489c216b136695aa61eb849
denbakal <denbakal7@gmail.com>
2018-12-24 12:44:53 +0200

    Load Balancing: Spring Hystrix + Feign

--- order-service/src/main/java/ua/demo/cloud/order/fallback/PaymentClientFallback.java
+++ order-service/src/main/java/ua/demo/cloud/order/fallback/PaymentClientFallback.java
@@ -2,8 +2,8 @@ package ua.demo.cloud.order.fallback;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
+import ua.demo.cloud.order.client.PaymentClient;
 import ua.demo.cloud.order.dto.PaymentDto;
-import ua.demo.cloud.order.feign.PaymentClient;
 
 @Log4j2
 @Component
