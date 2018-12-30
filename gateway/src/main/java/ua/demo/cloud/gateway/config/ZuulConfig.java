bc8b8a9ddfae5c8b2120d5b7e0fee666fa5d6251
denbakal <denbakal7@gmail.com>
2018-12-30 10:57:00 +0200

    API Gateway: Spring Zuul with Service Discovery

---
+++ gateway/src/main/java/ua/demo/cloud/gateway/config/ZuulConfig.java
@@ -0,0 +1,13 @@
+package ua.demo.cloud.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ua.demo.cloud.gateway.filters.SimpleFilter;
+
+@Configuration
+public class ZuulConfig {
+    /*@Bean
+    public SimpleFilter simpleFilter() {
+        return new SimpleFilter();
+    }*/
+}
